1fe6463b97a95500e130d9250b48f942e7fdac5d
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2022-05-29 14:00:18 +0300

    Implement factory class

--- src/dp/infra/factory.py
+++ src/dp/infra/factory.py
@@ -1,15 +1,104 @@
-from dp.core.spark import Spark, Job
+import argparse
+import importlib
+from itertools import accumulate
+
+import yaml
+from pyspark import SparkConf, SparkContext
+
+from dp.core.spark import Spark, Job, Source, Sink
 
 
 class Factory:
     """ Factory class for all objects """
 
     def __init__(self, argv):
-        self.__argv = argv
-        self.__config = {}
+        full_conf = self._parse_conf(file='application.yaml')
+        self._job_name = self._parse_job_name(argv, full_conf)
+        self._conf = full_conf[self._job_name]
+        self._argv = argv
 
     def create_spark(self) -> Spark:
         """ Create PySpark runner """
 
+        conf = SparkConf()
+        conf.set('spark.sql.session.timeZone', 'UTC')
+        properties = self._conf.get('properties', {})
+        for key, value in properties.items():
+            conf.set(key, value)
+
+        sc = SparkContext(conf=conf)
+
+        from dp.infra.aws.glue import GlueSpark  # pylint: disable=import-outside-toplevel
+        return GlueSpark(sc)
+
     def create_job(self) -> Job:
         """ Create instance of Job """
+
+        kwargs = self._parse_kwargs(self._conf)
+        module = importlib.import_module(f'dp.core.job.{self._job_name}')
+        job = getattr(module, self._job_name.title().replace('_', ''))
+        return job(**kwargs)
+
+    def _create_source(self, name, conf) -> Source:  # pylint: disable=no-self-use
+        raise ValueError(f'Unsupported source: {name}')
+
+    def _create_sink(self, name, conf) -> Sink:  # pylint: disable=no-self-use
+        raise ValueError(f'Unsupported sink: {name}')
+
+    def _create_decorator(self, name, conf):  # pylint: disable=unused-argument
+        kwargs = self._parse_kwargs(conf)
+        *module, cls = conf['class'].split('.')
+        module = importlib.import_module('.'.join(module))
+        decorator = getattr(module, cls)
+        return decorator(**kwargs)
+
+    def _parse_kwargs(self, super_conf):
+        kwargs = {}
+        for name, conf in super_conf['kwargs'].items():
+            if isinstance(conf, dict) and '__create' in conf:
+                method = getattr(self, f'_create_{conf["__create"]}')
+                kwargs[name] = method(name, conf)
+            else:
+                kwargs[name] = conf
+
+        return kwargs
+
+    @staticmethod
+    def _parse_job_name(argv, conf) -> str:
+        job_name = Factory._parse_argv(argv, 'JOB_NAME')
+        variants = accumulate(job_name.split('-')[::-1], lambda *s: '_'.join(s[::-1]))
+        for name in variants:
+            if name in conf:
+                return name
+
+        raise ValueError('Not recognized')
+
+    @staticmethod
+    def _parse_argv(argv, option) -> str:
+        parser = _ArgumentParser()
+        parser.add_argument('--' + option, required=True)
+        parsed, _ = parser.parse_known_args(argv[1:])
+        return vars(parsed)[option.replace('-', '_')]
+
+    @staticmethod
+    def _parse_conf(file) -> dict:
+        def merge(src, dst):
+            for key, value in src.items():
+                if isinstance(value, dict):
+                    node = dst.setdefault(key, {})
+                    merge(value, node)
+                else:
+                    dst[key] = value
+            return dst
+
+        with open(file, 'r', encoding='utf-8') as f:
+            conf = {}
+            for doc in yaml.safe_load_all(f):
+                conf = merge(doc, conf)
+
+        return conf
+
+
+class _ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise ValueError(message)
---
+++ tests/unit/dp/infra/test_factory.py
@@ -0,0 +1,200 @@
+import sys
+from unittest import mock
+
+import pytest
+
+from dp.core.spark import Job, Spark
+from dp.infra.factory import Factory
+
+
+# pylint: disable=protected-access
+def test_create_spark():
+    mock_factory = mock.Mock()
+    mock_factory._conf = {'properties': {'foo': '123'}}
+    mock_module = mock.Mock()
+    mock_glue_spark = mock.Mock()
+    mock_module.GlueSpark.return_value = mock_glue_spark
+    with mock.patch.dict('sys.modules', {'dp.infra.aws.glue': mock_module}):
+        actual = Factory.create_spark(mock_factory)
+
+        assert actual == mock_glue_spark
+        mock_module.GlueSpark.assert_called_once()
+        sc = mock_module.GlueSpark.call_args[0][0]
+        assert not sc._jsc.sc().isStopped()
+        conf = sc.getConf()
+        assert conf.get('spark.sql.session.timeZone') == 'UTC'
+        assert conf.get('foo') == '123'
+
+
+def test_create_job():
+    mock_factory = mock.Mock()
+    mock_factory._parse_kwargs.return_value = {'arg1': '123', 'arg2': 'qwerty'}
+    mock_factory._job_name = 'embedded_test_job'
+    mock_factory._conf = {'kwargs': {'foo': 123}}
+    with mock.patch('importlib.import_module') as mock_import_module:
+        mock_import_module.return_value = sys.modules[__name__]
+
+        actual = Factory.create_job(mock_factory)
+
+        assert isinstance(actual, EmbeddedTestJob)
+        assert actual.arg1 == '123'
+        assert actual.arg2 == 'qwerty'
+        mock_factory._parse_kwargs.assert_called_once_with({'kwargs': {'foo': 123}})
+
+
+def test_create_job_unknown_args():
+    mock_factory = mock.Mock()
+    mock_factory._parse_kwargs.return_value = {'arg1': '123', 'arg2': 'qwerty', 'arg3': 'foo'}
+    mock_factory._job_name = 'embedded_test_job'
+    with mock.patch('importlib.import_module') as mock_import_module:
+        mock_import_module.return_value = sys.modules[__name__]
+
+        with pytest.raises(TypeError) as _:
+            Factory.create_job(mock_factory)
+
+
+def test_create_source():
+    pass
+
+
+def test_create_sink():
+    pass
+
+
+def test_create_decorator():
+    mock_factory = mock.Mock()
+    mock_factory._parse_kwargs.return_value = {'arg1': '123', 'arg2': 'qwerty'}
+    config = {
+        'class': 'unit.dp.infra.test_factory.EmbeddedTestDecorator',
+        'kwargs': {
+            'foo': 321
+        }
+    }
+
+    actual = Factory._create_decorator(mock_factory, 'dummy', config)
+
+    assert isinstance(actual, EmbeddedTestDecorator)
+    assert actual.arg1 == '123'
+    assert actual.arg2 == 'qwerty'
+    mock_factory._parse_kwargs.assert_called_once_with(config)
+
+
+def test_parse_kwargs():
+    mock_factory = mock.Mock()
+    mock_factory._create_type1.return_value = 'abc'
+    mock_factory._create_type2.return_value = {'a': 'b'}
+    config = {
+        'kwargs': {
+            'foo': 123,
+            'bar': {'__create': 'type1'},
+            'baz': {'__create': 'type2'},
+            'tar': {'far': 123}
+        }
+    }
+
+    actual = Factory._parse_kwargs(mock_factory, config)
+
+    mock_factory._create_type1.assert_called_once_with('bar', {'__create': 'type1'})
+    mock_factory._create_type2.assert_called_once_with('baz', {'__create': 'type2'})
+    assert actual == {
+        'foo': 123,
+        'bar': 'abc',
+        'baz': {'a': 'b'},
+        'tar': {'far': 123}
+    }
+
+
+@pytest.mark.parametrize('sample, expected', [
+    ('foo-job', 'foo_job'),
+    ('vb-dev-foo-job', 'foo_job'),
+    ('vb-pre-dev-foobar-job', 'foobar_job'),
+    ('etl-prod-foo-bar-job', 'foo_bar_job'),
+    # pylint: disable-next=fixme
+    ('etl-prod-bar-foo-job', 'foo_job')  # FIXME: should be bar_foo_job ???
+])
+def test_parse_job_name(sample, expected):
+    argv = ['./test.py', f'--JOB_NAME={sample}', '--bar=baz']
+    conf = {'foo_job': '', 'foo_bar_job': '', 'foobar_job': '', 'bar_foo_job': ''}
+
+    actual = Factory._parse_job_name(argv, conf)
+
+    assert actual == expected
+
+
+def test_parse_job_name_no_recognized():
+    argv = ['./test.py', '--JOB_NAME=tar-job', '--bar=baz']
+    conf = {'foo_job': '', 'foo_bar_job': '', 'foobar_job': '', 'bar_foo_job': ''}
+
+    with pytest.raises(ValueError) as _:
+        Factory._parse_job_name(argv, conf)
+
+
+def test_parse_argv():
+    actual = Factory._parse_argv(['./test.py', '--foo=123', '--bar=baz'], 'foo')
+
+    assert actual == '123'
+
+
+def test_parse_argv_missed():
+    with pytest.raises(ValueError) as _:
+        Factory._parse_argv(['./test.py', '--foo=123', '--bar=baz'], 'tag')
+
+
+def test_parse_argv_with_dash():
+    actual = Factory._parse_argv(['./test.py', '--foo=123', '--bar-tar=baz'], 'bar-tar')
+
+    assert actual == 'baz'
+
+
+def test_parse_conf():
+    data = """
+foo: 123
+bar: baz
+tar:
+  far: false
+  bar: abc
+---
+bar: new
+tar:
+  bar: ABC
+  a:
+    b: 0
+baz: qwerty
+tor:
+  - 12
+  - 23
+    """
+    with mock.patch('builtins.open', mock.mock_open(read_data=data)) as mock_open_read:
+        actual = Factory._parse_conf('app.yaml')
+
+        mock_open_read.assert_called_once_with('app.yaml', 'r', encoding='utf-8')
+        assert actual == {
+            'foo': 123,
+            'bar': 'new',
+            'tar': {
+                'far': False,
+                'bar': 'ABC',
+                'a': {'b': 0}
+            },
+            'baz': 'qwerty',
+            'tor': [12, 23]
+        }
+
+
+class EmbeddedTestDecorator:  # pylint: disable=too-few-public-methods
+    """ Internal class used for tests """
+
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+
+class EmbeddedTestJob(Job):
+    """ Internal class used for tests """
+
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def transform(self, spark: Spark) -> None:
+        pass
